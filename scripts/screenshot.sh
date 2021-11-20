927695574d19a51c9d79813145700a63ce60a548
gourish <55114237+GustDevol@users.noreply.github.com>
2021-11-20 21:20:40 +0530

    removed unneeded files

--- scripts/screenshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-file_name="screenshot_$(date "+%Y-%m-%d_%T").png"
-output_dir="$HOME/Pictures/Screenshots"
-
-if [ $# -eq 0 ]; then
-        gnome-screenshot -f "$output_dir/$file_name"
-fi
